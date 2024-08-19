4c9db592f868cc7a0b362f8d2cbfc49522bfde6b
surachai <surachaikai2545@gmail.com>
2024-08-19 11:35:50 +0700

    set_state

--- app4/src/App.js
+++ app4/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import RefsFunc from "./res-func";
+import MessageBox from "./state-func";
 export default function App() {
-  return < RefsFunc />;
+  return < MessageBox />;
 }
