b14aa8d60a35e16b0d21b153566aca2899cf259a
surachai <surachaikai2545@gmail.com>
2024-09-02 10:56:51 +0700

    Contect&effect

--- app4/src/App.js
+++ app4/src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
-import RefsFunc from "./res-func";
-import MessageBox from "./state-func";
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-context2'
 export default function App() {
-  return < MessageBox />;
+  let [user, setUser] = React.useState();
+  return (<userContext.Provider value={[user, setUser]}>
+    <Header2 />
+    <Content2 />
+  </userContext.Provider>)
 }
